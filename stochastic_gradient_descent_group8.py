1b2874f5eb41b17c9fdf52b552a6915480df2e51
Kevin XU <kevin-xu@hotmail.fr>
2019-04-24 14:55:18 +0900

    ajout sigmoid function

---
+++ stochastic_gradient_descent_group8.py
@@ -0,0 +1,9 @@
+from matplotlib.pyplot import *
+from numpy import *
+from random import *
+from math import *
+
+# Import of other Python files
+from sigmoid_function_group8 import *
+
+print(sigmoid(1))
