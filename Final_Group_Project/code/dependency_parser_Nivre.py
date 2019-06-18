5f88e70891b95d9d0ee5b30d72c005c7f2054118
Kevin XU <kevin-xu@hotmail.fr>
2019-06-18 10:11:11 +0900

    fix print tree function

--- Final_Group_Project/code/dependency_parser_Nivre.py
+++ Final_Group_Project/code/dependency_parser_Nivre.py
@@ -9,84 +9,60 @@ import random as r
 import numpy as np
 from math import *
 
-# Trees - print trees
+# Library for trees
 from anytree import Node, RenderTree
-udo = Node(2)
-marc = Node(1, parent=udo)
-lian = Node("Liadazdan", parent=marc)
-dan = Node("Dadazdazn", parent=udo)
-jet = Node("Jedazdazt", parent=dan)
-jan = Node("Jadddn", parent=dan)
-joe = Node("Jdazdzaoe", parent=dan)
-
-L = [udo, marc]
-print(L)
-# print(marc)
-# Node('/Udo')
-# print(joe)
-# Node('/Udo/Dan/Joe')
-
-# for pre, fill, node in RenderTree(udo):   
-#   print("%s%s" % (pre, node.name))
-
-from anytree.exporter import DotExporter
-# # graphviz needs to be installed for the next line!
-# DotExporter(udo).to_picture("udo.png")
+from anytree.exporter import DotExporter # graphviz needs to be installed for the next line
 
 
+# # Help function for pre_process()
+# def has_digit(word):
+#     for char in word:
+#         if char.isdigit():
+#             return True
+#     return False
 
+# '''
+# Input: textfile in form of string
+# Output: List of list of words [[sentence1], [sentence2],...]
+# Side-effect: Removes some unwanted characters and words with numbers.
+# '''
+# def pre_process(text):
+#     filtered = ""
+#     for char in text:
+#         if char not in BAD_CHARS:
+#             if char == '.': # Splits '.' from sentence but still want to know where sentence ends.
+#                 char = " " + char
+#             filtered = filtered + char
+#     filtered_list = filtered.split()
+#     sentences = []
+#     sentence = []
+#     for word in filtered_list:
+#         if not has_digit(word):
+#             if word == '.':
+#                 sentences.append(sentence)
+#                 sentence = []
+#             else:
+#                 sentence.append(word)
+#     return sentences
 
-BAD_CHARS = [',', '<', '>', '!', '?', '-','<', ':',';','*','(',')','[',']','`','\'','"','는 ','은 ','과 ','이 ','그','저','가 ','을 ','를 ','에 ','와','나','로','의 ','도 ','께']
+# '''
+# Input: List of sentences
+# Output: Look-up tables for converting a word to int and vice versa.
+# '''
+# def create_dictionary(list_of_sentences):
+#     words = []
+#     for sentence in list_of_sentences:
+#         for word in sentence:
+#             if word not in words:
+#                 words.append(word)
 
-# Help function for pre_process()
-def has_digit(word):
-    for char in word:
-        if char.isdigit():
-            return True
-    return False
+#     int2word = {}
+#     word2int = {}
+#     for i, word in enumerate(words):
+#         word2int[word] = i
+#         int2word[i] = word
 
-'''
-Input: textfile in form of string
-Output: List of list of words [[sentence1], [sentence2],...]
-Side-effect: Removes some unwanted characters and words with numbers.
-'''
-def pre_process(text):
-    filtered = ""
-    for char in text:
-        if char not in BAD_CHARS:
-            if char == '.': # Splits '.' from sentence but still want to know where sentence ends.
-                char = " " + char
-            filtered = filtered + char
-    filtered_list = filtered.split()
-    sentences = []
-    sentence = []
-    for word in filtered_list:
-        if not has_digit(word):
-            if word == '.':
-                sentences.append(sentence)
-                sentence = []
-            else:
-                sentence.append(word)
-    return sentences
-
-'''
-Input: List of sentences
-Output: Look-up tables for converting a word to int and vice versa.
-'''
-def create_dictionary(list_of_sentences):
-    words = []
-    for sentence in list_of_sentences:
-        for word in sentence:
-            if word not in words:
-                words.append(word)
-
-    int2word = {}
-    word2int = {}
-    for i, word in enumerate(words):
-        word2int[word] = i
-        int2word[i] = word
-
-    return int2word, word2int
+#     return int2word, word2int
 
 
 
@@ -107,7 +83,6 @@ def reduce(S):
     return S
 
 def shift(S, I):
-    # smth = remove(I[0])  
     S.append(I.pop())
     return S
 
@@ -117,37 +92,16 @@ Output: The dependency parser of the sentence as a parsing tree
 '''
 def NivreParser(sentence):
     # Initialisation of the parser configuration
-    sentence.reverse() # to transform into a stack
     # print(sentence)
     n = len(sentence)
     S = [0] # We start with root on the stack
     I = [k for k in range(n)]
-    A = [] # arcs
+    A = [] # Arcs
 
     while (len(I) != 0):
-        # if len(S) == 0:
-        #     shift(S, I)
-
         topStack = S[len(S)-1]
         topBuffer = I[len(I)-1]
         noAction = True
-        # for i in range(len(A)):
-        #     # if A[i][0] == topBuffer and A[i][1] == topStack:
-        #         leftArc(S, I, A)
-        #         noAction = False
-        #         break
-        #     elif A[i][0] == topStack and A[i][1] == topBuffer:
-        #         hasChildren = False
-        #         for j in range(len(A)):
-        #             if A[j][0] == topBuffer:
-        #                 hasChildren = True
-        #         if hasChildren == True:
-        #             rightArc(S, i, A)
-        #             noAction = False
-        #         break
-        #     elif A[i][1] == topStack:
-        #         reduce(S)
-        #         noAction 
 
         hasParent = False
         for i in range(len(A)):
@@ -175,11 +129,30 @@ def NivreParser(sentence):
 
     return A
 
+'''
+Input: A the arcs in the tree and the sentence 
+Output: Generate a PNG file of the tree
+'''
+def printTree(A, sentence):
+    # Creation tree -> to review
+    print(sentence)
+    tree = [Node("root")]
+    for i in range(len(sentence)):
+        tree.append(Node(sentence[i]))
+    tree[1] = Node(sentence[A[0][0]], parent=tree[0])
+
+    for i in range(len(A)):
+        tree[A[i][1]+1] = Node(sentence[A[i][1]], parent = tree[A[i][0]+1]) 
+    # print(tree)
+
+    # for pre, fill, node in RenderTree(tree[len(tree)-1]):   
+    #     print("%s%s" % (pre, node.name))
+    print(tree)
+    DotExporter(tree[0]).to_picture("tree.png")
+
 def main():
     data = "My name is Nivre from South Korea."
-    data = "새 생명 ."
-    rules = "(NP (DP 새/MM) (NP 생명/NNG + ./SF))"
-    print(data.split())
+    data = "몇 시가 되었는지 경지는 모르고 있었다 ."
 
     # sentences = pre_process(data)
 
@@ -196,21 +169,13 @@ def main():
 
     # Nivre Parser
     parser = NivreParser(s1)
+    parser = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+
     print(parser)
 
+    printTree(parser, s1)
 
-    # Creation tree -> to review
-    tree = []
-    for i in range(len(s1)):
-        tree.append(Node(s1[i]))
-
-    for i in range(len(parser)):
-        tree[parser[i][1]] = Node(s1[parser[i][1]], parent = tree[parser[i][0]]) 
-    # print(tree)
-
-    # for pre, fill, node in RenderTree(tree[len(tree)-1]):   
-    #     print("%s%s" % (pre, node.name))
-    DotExporter(tree[len(tree)-1]).to_picture("tree.png")
+    
 
 
 if __name__ == '__main__':
