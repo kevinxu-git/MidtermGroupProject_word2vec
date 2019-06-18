1dab12b934747951fb5cc1bc62b17dce24af2abf
antoneklund <antoneklund1@hotmail.com>
2019-06-18 11:56:27 +0900

    added import and oracle.py

--- Final_Group_Project/code/dependency_parser_Nivre.py
+++ Final_Group_Project/code/dependency_parser_Nivre.py
@@ -1,4 +1,6 @@
-# NLP Final Group Project : Dependency parser of Nivre for the Korean Language version 
+#-*- coding:utf-8 -*-
+
+# NLP Final Group Project : Dependency parser of Nivre for the Korean Language version
 # Yonsei University
 # Groupe 8
 
@@ -9,74 +11,79 @@ import random as r
 import numpy as np
 from math import *
 
+import os
+import codecs as c
+from io import StringIO, BytesIO
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
+
 # Library for trees
 from anytree import Node, RenderTree
 from anytree.exporter import DotExporter # graphviz needs to be installed for the next line
 
 
-# # Help function for pre_process()
-# def has_digit(word):
-#     for char in word:
-#         if char.isdigit():
-#             return True
-#     return False
+def extract_body(data):
+    body = ""
+    save = False
+    for i, char in enumerate(data):
+        if char == '<':
+            check = data[i:i+6]
+            if check == "<body>":
+                save = True
+            if check == "</body":
+                save = False
+        if save and char != '\t':
+            body = body  + char
+    return body[6:]
 
-# '''
-# Input: textfile in form of string
-# Output: List of list of words [[sentence1], [sentence2],...]
-# Side-effect: Removes some unwanted characters and words with numbers.
-# '''
-# def pre_process(text):
-#     filtered = ""
-#     for char in text:
-#         if char not in BAD_CHARS:
-#             if char == '.': # Splits '.' from sentence but still want to know where sentence ends.
-#                 char = " " + char
-#             filtered = filtered + char
-#     filtered_list = filtered.split()
-#     sentences = []
-#     sentence = []
-#     for word in filtered_list:
-#         if not has_digit(word):
-#             if word == '.':
-#                 sentences.append(sentence)
-#                 sentence = []
-#             else:
-#                 sentence.append(word)
-#     return sentences
+def parse(body):
+    # print(body)
+    sentences = []
+    sentence_add = False
+    sentence = ""
+    dictionary = []
+    dict_add = False
 
-# '''
-# Input: List of sentences
-# Output: Look-up tables for converting a word to int and vice versa.
-# '''
-# def create_dictionary(list_of_sentences):
-#     words = []
-#     for sentence in list_of_sentences:
-#         for word in sentence:
-#             if word not in words:
-#                 words.append(word)
+    for i, char in enumerate(body):
+        if char == '\r':
+            if sentence_add:
+                sentences.append(sentence)
+                dictionary.append(dict)
+                sentence = ""
+                sentence_add = False
+        if sentence_add:
+            sentence = sentence + char
+        if char == ';' and body[i+1] == ' ':
+            sentence = ""
+            sentence_add = True
+    #clear all with 'Q'
+    new = []
+    for i, s in enumerate(sentences):
+        # print(s)
+        if 'Q' not in s:
+            new.append(s)
+    return new
 
-#     int2word = {}
-#     word2int = {}
-#     for i, word in enumerate(words):
-#         word2int[word] = i
-#         int2word[i] = word
-
-#     return int2word, word2int
+def add_tags(sentences):
+    sentence_dict = []
+    kkma = Kkma()
+    for i, sent in enumerate(sentences):
+        sentence_dict.append((sent, kkma.pos(sent)))
+    return sentence_dict
 
 
 
 # Functions for arc-earger parsing algorithm : leftArc, rightArc, reduce, shift
 def leftArc(S, I, A):
-    A.append([I[len(I)-1], S.pop()]) # Add the arc (j,i) to A 
+    A.append([I[len(I)-1], S.pop()]) # Add the arc (j,i) to A
     # S.pop() # Pop the stack
     return 0
-  
+
 def rightArc(S, I, A):
     A.append([S[len(S)-1], I[len(I)-1]]) # Add an arc from wi to wj from the token wi on top of the stack to the next input token wj
     S.append(I[len(I)-1]) # Push wj onto S.
-    return 0              
-  
+    return 0
+
 def reduce(S):
     S.pop() # Pop the stack S
     return S
@@ -87,7 +94,7 @@ def shift(S, I):
 
 '''
 Input: A sentence
-Output: The dependency parser of the sentence as a parsing tree 
+Output: The dependency parser of the sentence as a parsing tree
 '''
 def NivreParser(sentence):
     # Initialisation of the parser configuration
@@ -128,7 +135,7 @@ def NivreParser(sentence):
     return A
 
 '''
-Input: A the arcs in the tree and the sentence 
+Input: A the arcs in the tree and the sentence
 Output: Generate a PNG file of the tree
 '''
 def printTree(A, sentence):
@@ -140,24 +147,24 @@ def printTree(A, sentence):
     tree[1] = Node(sentence[A[0][0]], parent=tree[0])
 
     for i in range(len(A)):
-        tree[A[i][1]+1] = Node(sentence[A[i][1]], parent = tree[A[i][0]+1]) 
+        tree[A[i][1]+1] = Node(sentence[A[i][1]], parent = tree[A[i][0]+1])
 
     print(tree)
     DotExporter(tree[0]).to_picture("tree.png")
     return 0
 
 def main():
+    '''
+    Functions for importing corpus and extracting sentences+tags_dict
+    dir = os.getcwd()
+    raw = c.open(dir + "/dataset/BGEO0292.txt", "rb", "utf-16")
+    data = raw.read() # to be able to manipulate input
+    body = extract_body(data)    #extract sentences
+    sentences = parse(body)      #extract tags
+    sentence_dict = add_tags(sentences) #sentence_dict = [sentence, [(word, tag)]
+    '''
+
     data = "몇 시가 되었는지 경지는 모르고 있었다 ."
-
-    # sentences = pre_process(data)
-
-    # print(sentences, end = "\n\n")
-
-    # dictionary = create_dictionary(sentences)
-    # int2word, word2int = dictionary
-    # vocab_size = len(int2word)
-    # print("Number of unique words = ", vocab_size, end = "\n")
-    # # print(dictionary, end = "\n\n")
 
     s1 = data.split()
     print(s1)
@@ -171,5 +178,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
